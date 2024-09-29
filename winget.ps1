f6486242ed767a4e4c782c0ea14679ea73d72ef9
ViChanzo <75827403+vichanzo@users.noreply.github.com>
2024-09-29 01:24:50 -0700

    Create winget.ps1

---
+++ winget.ps1
@@ -0,0 +1,27 @@
+#!/usr/bin/env powershell
+#https://github.com/muradbu/winget-script/blob/main/winget.ps1
+
+winget install -e --id 7zip.7zip;
+winget install -e --id Bitwarden.Bitwarden;
+winget install -e --id CPUID.CPU-Z;
+winget install -e --id TechPowerUp.GPU-Z;
+
+winget install -e --id Microsoft.Teams;
+
+winget install -e --id Microsoft.VisualStudioCode;
+winget install -e --id Microsoft.WindowsTerminal;
+
+winget install -e --id Mozilla.Firefox.DeveloperEdition;
+winget install -e --id eloston.ungoogled-chromium;
+
+winget install -e --id Rufus.Rufus;
+
+winget install -e --id OBSProject.OBSStudio;
+winget install -e --id OpenJS.NodeJS.LTS;
+winget install -e --id Oracle.JavaRuntimeEnvironment;
+winget install -e --id Oracle.JDK.17;
+
+winget install -e --id Python.Python;
+winget install -e --id Tutanota.Tutanota;
+
+winget install -e --id VideoLAN.VLC;
